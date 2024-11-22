32393c85819779ca12941116987512d150dffa08
Marlon <marlon.john@hotmail.de>
2024-11-22 14:46:05 +0100

    better testing for gh workflow

--- src/Zlib-ng.NET.Tests/Tests.cs
+++ src/Zlib-ng.NET.Tests/Tests.cs
@@ -1,14 +1,20 @@
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Net;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace ZlibngDotNet.Tests;
 
-public class Tests
+public class Tests : IAsyncLifetime
 {
-	private static readonly Zlibng Zlib = new(Path.Combine(
-		Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
-		@"Libraries\zlib-ng2.dll"));
+	private string _filePath;
+	private Zlibng _zlibng;
+
+	public async Task InitializeAsync()
+	{
+		_filePath = await DownloadAsync();
+		_zlibng = new Zlibng(_filePath);
+	}
 
 	[Fact]
 	public void CompressAndDecompress()
@@ -16,16 +22,16 @@ public class Tests
 		var randomString = GetRandomString(8192);
 		var randomStringBuffer = Encoding.ASCII.GetBytes(randomString);
 
-		var compressedBufferSize = (int)Zlib.CompressBound(randomStringBuffer.Length);
+		var compressedBufferSize = (int)_zlibng.CompressBound(randomStringBuffer.Length);
 		Assert.NotEqual(0, compressedBufferSize);
 
 		var compressedBuffer = new byte[compressedBufferSize];
-		var compressionResult = Zlib.Compress(compressedBuffer, randomStringBuffer, out int compressedSize);
+		var compressionResult = _zlibng.Compress(compressedBuffer, randomStringBuffer, out int compressedSize);
 		Assert.Equal(ZlibngCompressionResult.Ok, compressionResult);
 		Assert.NotEqual(0, compressedSize);
 
 		var decompressedBuffer = new byte[randomStringBuffer.Length];
-		var decompressionResult = Zlib.Uncompress(decompressedBuffer, compressedBuffer.AsSpan(0, compressedSize), out int decompressedSize);
+		var decompressionResult = _zlibng.Uncompress(decompressedBuffer, compressedBuffer.AsSpan(0, compressedSize), out int decompressedSize);
 		Assert.Equal(ZlibngCompressionResult.Ok, decompressionResult);
 		Assert.NotEqual(0, decompressedSize);
 
@@ -33,18 +39,53 @@ public class Tests
 		Assert.True(randomStringBuffer.AsSpan().SequenceEqual(decompressedBuffer));
 	}
 
+	public Task DisposeAsync()
+	{
+		_zlibng.Dispose();
+		File.Delete(_filePath);
+		return Task.CompletedTask;
+	}
+
 	private static string GetRandomString(int length)
 	{
-		const string chars = "0123456789ABCDEF";
-		var result = new string('\0', length);
-		var resultReadonlySpan = result.AsSpan();
-		var resultSpan = MemoryMarshal.CreateSpan(ref Unsafe.AsRef(in resultReadonlySpan.GetPinnableReference()), length);
+		const string pool = "0123456789ABCDEF";
+		return RandomNumberGenerator.GetString(pool, length);
+	}
 
-		for (var i = 0; i < resultSpan.Length; i++)
+	public static async Task<string> DownloadAsync()
+	{
+		if (!OperatingSystem.IsWindows() && !OperatingSystem.IsLinux())
 		{
-			resultSpan[i] = chars[Random.Shared.Next(chars.Length)];
+			throw new PlatformNotSupportedException("this test is not supported on the current platform");
 		}
 
-		return result;
+		const string baseUrl = "https://github.com/NotOfficer/Zlib-ng.NET/releases/download/1.0.0/";
+		string url;
+
+		if (OperatingSystem.IsWindows())
+		{
+			url = baseUrl + "zlib-ng2.dll";
+		}
+		else if (OperatingSystem.IsLinux())
+		{
+			url = baseUrl + "libz-ng.so";
+		}
+		else
+		{
+			throw new UnreachableException();
+		}
+
+		using var client = new HttpClient(new SocketsHttpHandler
+		{
+			UseProxy = false,
+			UseCookies = true,
+			AutomaticDecompression = DecompressionMethods.All
+		});
+		using var response = await client.GetAsync(url);
+		response.EnsureSuccessStatusCode();
+		var filePath = Path.GetTempFileName();
+		await using var fs = File.Create(filePath);
+		await response.Content.CopyToAsync(fs);
+		return filePath;
 	}
 }
